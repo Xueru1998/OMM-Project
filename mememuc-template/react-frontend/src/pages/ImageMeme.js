f82b55b73c05e288818cdaf181b148dbf5bca3ee
Xueru Zheng <zhengxu@cip.ifi.lmu.de>
2022-12-15 02:26:53 +0100

    delete unused

--- mememuc-template/react-frontend/src/pages/ImageMeme.js
+++ mememuc-template/react-frontend/src/pages/ImageMeme.js
@@ -2,7 +2,7 @@ import React from "react";
 import Search from "./Search";
 import Gallery from "./Gallery";
 import Decorate from "./Decorate";
-import Generate from "./Generate";
+
 import { MainImage, InputGroup } from "./Upload";
 
 import "bootstrap/dist/css/bootstrap.min.css";
