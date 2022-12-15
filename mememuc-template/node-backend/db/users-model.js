6857b4cf2267bce9bf39bd9c36ff9c1e81852ec4
Xueru Zheng <zhengxu@cip.ifi.lmu.de>
2022-12-15 16:17:45 +0100

    can add image to data base

--- mememuc-template/node-backend/db/users-model.js
+++ mememuc-template/node-backend/db/users-model.js
@@ -45,22 +45,38 @@ module.exports = User;
  *  https://www.section.io/engineering-education/nodejs-mongoosejs-mongodb/
  */
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: false,
-    },
-    password: {
-        type: String,
-        required: true,
-    }
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: false,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+const User = mongoose.model("User", userSchema);
+
+const student2 = new User({
+  username: "student1",
+  email: "123@qq.com",
+  password: "123",
 });
 
-const user = mongoose.model('user', userSchema);
-module.exports = user;
+student2
+  .save()
+  .then(() => {
+    console.log("student has been saved to database");
+  })
+  .catch((e) => {
+    console.log("error has happened");
+    console.log(e);
+  });
+
+module.exports = User;
