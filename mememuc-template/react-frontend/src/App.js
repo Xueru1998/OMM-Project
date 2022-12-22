d39f051fcfab2d752789aa16a3bb8dcb394fcd2f
Xueru Zheng <zhengxu@cip.ifi.lmu.de>
2022-12-22 15:14:22 +0100

    delete files due to group changing

--- mememuc-template/react-frontend/src/App.js
+++ mememuc-template/react-frontend/src/App.js
@@ -2,10 +2,7 @@ import "./styles/App.css";
 import Nav from "./pages/Nav";
 import ImageMeme from "./pages/ImageMeme";
 import About from "./pages/About";
-import Overview from "./pages/Overview";
-import SingleView from "./pages/SingleView";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import HomePage from "./pages/Post";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,13 +10,9 @@ function App() {
     <div className="App">
       <Nav />
       <Routes>
-        <Route path="/mememaker" element={<ImageMeme />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
-          <Route path="/" element={<Overview />} />
-          <Route path='/meme' element={<SingleView />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-
+        <Route path="/ImageMeme" element={<ImageMeme />} />
       </Routes>
     </div>
   );
--- mememuc-template/react-frontend/src/pages/Nav.js
+++ mememuc-template/react-frontend/src/pages/Nav.js
@@ -1,62 +1,21 @@
-
-/**
- * References for Navbar:
- * https://react-bootstrap.github.io/components/navbar/
- * */
-
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from '../pic/logo.jpg';
-
-function NavigationBar() {
+import { Link } from "react-router-dom";
+//the navigation bar "about" and "home"
+const Nav = () => {
   return (
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="/">
-            <img
-                src={logo}
-                alt="HOME"
-            width="50"
-            height="50"/></Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
-                navbarScroll
-            >
-              <NavDropdown title="Create" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="mememaker">Make a Meme</NavDropdown.Item>
-                <NavDropdown.Item href="gifmaker">Make a Gif</NavDropdown.Item>
-                <NavDropdown.Item href="videomaker">Make a Video</NavDropdown.Item>
-              </NavDropdown>
-
-
-              <Form className="d-flex">
-                <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                />
-                <Button variant="light">Search</Button>
-              </Form>
-            </Nav>
-            <Nav>
-              <Nav.Link href="about">About</Nav.Link>
-              <Nav.Link href="login">Login</Nav.Link>
-            </Nav>
-
-
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">HomePage</Link>
+        </li>
+        <li>
+          <Link to="/about">about</Link>
+        </li>
+        <li>
+          <Link to="/ImageMeme">Make an image Meme</Link>
+        </li>
+      </ul>
+    </nav>
   );
-}
-
-export default NavigationBar;
+};
+export default Nav;
